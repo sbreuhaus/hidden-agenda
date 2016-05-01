96334155763716b87a240a7f6c1117ea99aa09cb
Alexandra Breuhaus <sbreuhaus@gmail.com>
2016-05-01 14:00:46 -0400

    Home

--- front-end/app/components/Home.js
+++ front-end/app/components/Home.js
@@ -11,3 +11,5 @@ const Home = React.createClass({
     )
   }
 })
+
+export default Home;
--- front-end/app/config/routes.js
+++ front-end/app/config/routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import Main from '../components/Main';
 import Home from '../components/Home';
+
 
 const routes = (
   <Router history={hashHistory}>
