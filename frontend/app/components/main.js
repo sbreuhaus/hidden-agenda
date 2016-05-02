ffb4f8fd7727498aa564130922686794d84bb1c6
Sumit <GETSUMO@Sumits-MacBook-Pro-2.local>
2016-05-02 13:31:19 -0400

    pushing updates

---
+++ frontend/app/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = React.createClass({
+  render: function() {
+    return (
+      <div className="main-container">
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+export default Main;
