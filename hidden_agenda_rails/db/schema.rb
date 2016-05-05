ad1037035081dc696699320d5e37290703853bd6
sungjin0426 <sjlbest0426@gmail.com>
2016-05-05 00:07:26 -0400

    updating schema, user, seed

--- hidden_agenda_rails/db/schema.rb
+++ hidden_agenda_rails/db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20160502153707) do
 
   create_table "photos", force: :cascade do |t|
     t.text     "photo_url"
+    t.integer  "user_id"
     t.string   "search_name"
     t.text     "description"
     t.datetime "created_at",  null: false
