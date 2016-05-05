d4f4d5229f0e37c635d6a0c7cbd0d7d314b487f3
sungjin0426 <sjlbest0426@gmail.com>
2016-05-05 00:10:24 -0400

    updating schema, user, seed[A

--- hidden_agenda_rails/db/migrate/20160502153114_create_photos.rb
+++ hidden_agenda_rails/db/migrate/20160502153114_create_photos.rb
@@ -1,7 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-      t.integer :photo_id
+      t.text :photo_url
+      t.integer :user_id
       t.string :search_name
       t.text :description
 
