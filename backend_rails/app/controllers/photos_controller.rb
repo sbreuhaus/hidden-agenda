90bbaacc5e22714d79f8876f8a4d624eaa0c37a2
Sumit <GETSUMO@Sumits-MacBook-Pro-2.local>
2016-05-06 00:34:29 -0400

    pushing empty to pull stephen

--- backend_rails/app/controllers/photos_controller.rb
+++
@@ -1,6 +0,0 @@
-class PhotosController < ApplicationController
-
-  def index
-    render json: Photo.all, status:200
-  end
-end
--- backend_rails/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules.
-  has_many :photos
-  
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
-          # :confirmable, :omniauthable
-  include DeviseTokenAuth::Concerns::User
-end
